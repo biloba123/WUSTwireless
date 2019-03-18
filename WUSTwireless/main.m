3a7d7105e211ea6a3b3550555796a4a704ebee69
吕晴阳 <lvqingyang@lvqingyangdemac-mini.local>
2019-03-18 15:34:00 +0800

    Initial Commit

---
+++ WUSTwireless/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WUSTwireless
+//
+//  Created by 吕晴阳 on 2019/3/18.
+//  Copyright © 2019 吕晴阳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
